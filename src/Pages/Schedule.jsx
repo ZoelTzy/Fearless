3f0226a3bafec3a7e1cc3a2612b18b3c03146145
ZoelTzy <103124907+ZoelTzy@users.noreply.github.com>
2025-04-21 16:17:52 +0700

    Update Schedule.jsx

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -17,10 +17,7 @@ const Schedule = () => {
         AOS.refresh()
     }, [])
 
-    let piketGroup = []
-
-    // Menentukan kelompok piket berdasarkan hari
-    piketGroup = [
+    let piketGroup = [
         ["Rifai", "Audrey", "Ghaby", "Dimas", "Angel", "Reza"],
         ["Aulia", "Altha", "Raehan", "Fitria", "Keenan", "Desvita"],
         ["Irma", "Zaki", "Hafiz", "Nafasat", "Jelita", "Marsya", "Maylyana"],
@@ -28,28 +25,12 @@ const Schedule = () => {
         ["Renata", "Rio", "Dicky", "Sellina", "Gilang", "Sherin"],
     ]
 
-    const dayComponents = [
-        null, // Kosongkan indeks 0
-        Senin,
-        Selasa,
-        Rabu,
-        Kamis,
-        Jumat,
-    ]
-
-    // Menampilkan komponen berdasarkan hari saat ini
+    const dayComponents = [null, Senin, Selasa, Rabu, Kamis, Jumat]
     const TodayComponent = dayComponents[new Date().getDay()]
-
-    // Menampilkan nama-nama piket sesuai dengan hari saat ini
     const currentPiketNames = piketGroup[new Date().getDay() - 1]
-
-    console.log("Current Day:", currentDay)
-    console.log("Piket Group:", piketGroup)
-    console.log("Current Piket Names:", currentPiketNames)
 
     return (
         <>
-            {/* Jadwal Mapel */}
             <div className="lg:flex lg:justify-center lg:gap-32 lg:mb-10 lg:mt-16 ">
                 <div className="text-white flex flex-col justify-center items-center mt-8 md:mt-3 overflow-y-hidden">
                     <div className="text-2xl font-medium mb-5" data-aos="fade-up" data-aos-duration="500">
@@ -67,23 +48,13 @@ const Schedule = () => {
                 </div>
             </div>
 
-            {/* Jadwal Piket */}
             <div className="text-white flex flex-col justify-center items-center mt-8 lg:mt-0 lg:mb-[10rem] mb-10 overflow-y-hidden">
-                <div
-                    className="text-2xl font-medium mb-5 text-center"
-                    data-aos="fade-up"
-                    data-aos-duration="500">
+                <div className="text-2xl font-medium mb-5 text-center" data-aos="fade-up" data-aos-duration="500">
                     Piket
                 </div>
                 {currentPiketNames && currentPiketNames.length > 0 ? (
                     currentPiketNames.map((piketName, index) => (
-                        <div
-                            key={index}
-                            className={` border-t-2 border-white flex justify-center py-[0.50rem] w-72 px-3 ${
-                                index === currentPiketNames.length - 1 ? "border-b-2" : ""
-                            }`}
-                            data-aos="fade-up"
-                            data-aos-duration={600 + index * 100}>
+                        <div key={index} className={` border-t-2 border-white flex justify-center py-[0.50rem] w-72 px-3 ${index === currentPiketNames.length - 1 ? "border-b-2" : ""}`} data-aos="fade-up" data-aos-duration={600 + index * 100}>
                             <div className="text-base font-medium">{piketName}</div>
                         </div>
                     ))
