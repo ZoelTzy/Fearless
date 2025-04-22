645ee8f737951a825c2114ea8e9842e480f1ee2f
ZoelTzy <103124907+ZoelTzy@users.noreply.github.com>
2025-04-22 17:49:40 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,12 +2,12 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://linktr.ee/fearless_classss">
+			<a href="https://ngl.link/fearlesss_class74395">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-base font-semibold pr-0 mt-5">Old Website</h1>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Ngl Class</h1>
 			</a>
 		</div>
 	)
